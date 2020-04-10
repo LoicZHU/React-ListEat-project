4c50e123b4922ca589cf09caff7ed6c81c56b006
LoicZHU <zhu.loic@gmail.com>
2020-04-10 10:38:56 +0200

    component Cancellation

---
+++ frontend/src/components/Cancellation/index.js
@@ -0,0 +1,21 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './cancellation.scss';
+
+// == Composant
+const Cancellation = () => (
+  <main className="ticket__cancellation">
+    <h1>Dommage !</h1>
+
+    <p>Vous avez annulé votre demande.</p>
+
+    <a className="ticket__cancellation__link" href="#">
+      Revenir sur la page d'inscription à la liste d'attente
+    </a>
+  </main>
+);
+
+// == Export
+export default Cancellation;
