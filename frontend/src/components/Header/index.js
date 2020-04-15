a5c2c6c00333a20d03d142ceea4154899eabe5bd
SylvainPayet <sylvain@studiogone.com>
2020-04-15 21:34:34 +0200

    mobile menu DONE

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -12,65 +12,103 @@ const Header = ({ isRestaurantLogged, handleLogout, restaurantId }) => {
   return (
     <header>
       <nav>
-      <ul id="mobile-nav">
-          <div id="menu-icon">
-            <li id="home-button-mobile" className="header-mobile-nav-button"><i class="fa fa-bars" aria-hidden="true"></i></li>
+      <div id="mobile-nav">
+        <div class="header">
+          <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu"/>
+          <label for="openSidebarMenu" class="sidebarIconToggle">
+            <div class="spinner diagonal part-1"></div>
+            <div class="spinner horizontal"></div>
+            <div class="spinner diagonal part-2"></div>
+          </label>
+
+          <div id="logo-container">
+            <a href="/"><img src={Logo}/></a>
           </div>
-          <div id="menu-wrapper">
-            <ul>
-            <a href="/"><li>Accueil</li></a>
-            <a href="/login"><li>Connexion</li></a>
-            <a href="/signup"><li>Inscription</li></a>
-            <a href="/faq"><li>Aide <span id="help-button-span">?</span> </li></a>
+
+          <div id="sidebarMenu">
+            <ul class="sidebarMenuInner">
+
+            <a href="/">
+              <li>Accueil</li>
+            </a>
+            <a href="/faq">
+              <li id="help">Aide <span id="help-button-span">?</span></li>
+            </a>
+              {/* if not logged */}
+              {!isRestaurantLogged && (
+                <a href="/signup"><li>Inscription</li></a>
+              )}
+
+              {/* if not logged */}
+              {!isRestaurantLogged && (
+                <a href="/login"><li>Connexion</li></a>
+              )}
+
+              {/* if logged */}
+              {isRestaurantLogged && (
+                <a href={`/partner/${restaurantId}/administration/`}>
+                  <li>Mon espace</li>
+                </a>
+              )}
+              {isRestaurantLogged && (
+                <a href={`/partner/${restaurantId}/administration/edit`}>
+                  <li>Mon profil</li>
+                </a>
+              )}
+
+              {isRestaurantLogged && (
+                <a onClick={handleClick}>
+                  <li>Déconnexion</li>
+                </a>
+              )}
             </ul>
           </div>
+      </div>
+    </div>
+    <div id="desktop-menu">
+      <ul id="desktop-left-nav">
+        <a href="/">
+          <li id="home-button" className="header-nav-button button">Accueil</li>
+        </a>
+        <a href="/faq">
+          <li id="help-button" className="header-nav-button button">Aide <span id="help-button-span">?</span></li>
+        </a>
       </ul>
-        <ul id="desktop-left-nav">
-          <a href="/">
-            <li id="home-button" className="header-nav-button button">Accueil</li>
-            <li id="home-button-mobile" className="header-mobile-nav-button"><i className="fa fa-home" aria-hidden="true"></i></li>
+      <div id="logo-container">
+        <a href="/"><img src={Logo}/></a>
+      </div>
+      <ul id="desktop-right-nav">
+        {/* if not logged */}
+        {!isRestaurantLogged && (
+          <a href="/signup"><li className="header-nav-button button">Inscription</li></a>
+        )}
+
+        {/* if not logged */}
+        {!isRestaurantLogged && (
+          <a href="/login"><li className="header-nav-button button">Connexion</li></a>
+        )}
+
+        {/* if logged */}
+        {isRestaurantLogged && (
+          <a href={`/partner/${restaurantId}/administration/`}>
+            <li id="help-button" className="header-nav-button button">Mon espace</li>
           </a>
-          <a href="/faq">
-            <li id="help-button" className="header-nav-button button">Aide <span id="help-button-span">?</span></li>
-            <li id="help-button-mobile">?</li>
+        )}
+        {isRestaurantLogged && (
+          <a href={`/partner/${restaurantId}/administration/edit`}>
+            <li id="help-button" className="header-nav-button button">Mon profil</li>
           </a>
-        </ul>
-        <div id="logo-container">
-          <a href="/"><img src={Logo}/></a>
-        </div>
+        )}
 
-        <ul id="desktop-right-nav">
-          {/* if not logged */}
-          {!isRestaurantLogged && (
-            <a href="/signup"><li className="header-nav-button button">Inscription</li></a>
-          )}
-
-          {/* if not logged */}
-          {!isRestaurantLogged && (
-            <a href="/login"><li className="header-nav-button button">Connexion</li></a>
-          )}
-
-          {/* if logged */}
-          {isRestaurantLogged && (
-            <a href={`/partner/${restaurantId}/administration/`}>
-              <li id="help-button" className="header-nav-button button">Mon espace</li>
-            </a>
-          )}
-          {isRestaurantLogged && (
-            <a href={`/partner/${restaurantId}/administration/edit`}>
-              <li id="help-button" className="header-nav-button button">Mon profil</li>
-            </a>
-          )}
-
-          {isRestaurantLogged && (
-            <a onClick={handleClick}>
-              <li className="header-nav-button button">Déconnexion</li>
-            </a>
-          )}
-        </ul>
-
-      </nav>
-    </header>
+        {isRestaurantLogged && (
+          <a onClick={handleClick}>
+            <li className="header-nav-button button">Déconnexion</li>
+          </a>
+        )}
+      </ul>
+    </div>
+    </nav>
+  </header>
   );
 };
 
