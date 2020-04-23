2d2a250db01aecbe68a72931dbb507a0728343bd
Liza-T-XLS <shapi94@gmail.com>
2020-04-23 17:27:35 +0200

    unit test: Timer EstimatedEntryTime

--- backend/tests/Services/TimerTest.php
+++ backend/tests/Services/TimerTest.php
@@ -15,4 +15,15 @@ class TimerTest extends TestCase
         // assert that the timer calculated the estimated time correctly!
         $this->assertEquals(20, $result);
     }
+
+    public function testEstimatedEntryTime()
+    {
+        $timer = new Timer();
+        // DateTime object: 2020-04-23T11:26:43+02:00
+        $result = $timer->estimatedEntryTime(20);
+        // purposefully formatted the result to only show the hours and minutes so it would be easy to change the expected result while running tests.
+        $formattedResult = date_format($result, 'H:i');
+        // expected result to change according to current time
+        $this->assertEquals("17:41", $formattedResult);
+    }
 }
