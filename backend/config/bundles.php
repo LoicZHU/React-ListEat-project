26b0841e1c34945d61e1d8b32ce514b5a92d41ed
Liza-T-XLS <shapi94@gmail.com>
2020-04-24 18:54:32 +0200

    reversed access control

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
