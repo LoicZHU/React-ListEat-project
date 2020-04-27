e3ab8b82196cd4fb34d0ca839cf31cab3fe8839e
SylvainPayet <sylvain@studiogone.com>
2020-04-27 13:18:20 +0200

    admin responsive DONE

--- frontend/src/components/Admin/index.js
+++ frontend/src/components/Admin/index.js
@@ -206,7 +206,8 @@ const Admin = ({
 
   return (
     (!loadingTicketsData && (
-      <div id="admin-wrapper">
+      <>
+      <div id="admin-mobile-wrapper">
 {/* 
          <Websocket url='ws://localhost:8001/mercure-test'
         onMessage={(data) => console.log(data)}
@@ -446,6 +447,248 @@ const Admin = ({
           </div>
         </div>
       </div>
+
+<div id="admin-desktop-wrapper">
+{/* 
+         <Websocket url='ws://localhost:8001/mercure-test'
+        onMessage={(data) => console.log(data)}
+        /> */}
+
+        {/* left section */}
+        <div id="left-section">
+          <div id="admin-top-section">
+            <div className="left">
+              <label className="switch">
+                <input type="checkbox" checked={status} onChange={handleServiceClick} />
+                <span className="slider round" />
+              </label>
+              <span className="toggle-name">SERVICE : </span>
+
+              {(status === 1) && <span className="toggle-state on">ON</span>}
+
+              {!(status === 1) && <span className="toggle-state off">OFF</span>}
+            </div>
+
+            <div className="right">
+              <span>{currentTime}</span>
+            </div>
+          </div>
+
+          <div id="admin-middle-section">
+            {tickets.length === 0 && (
+              <div id="empty-ticket-overlay">
+                <span>Pas de ticket en cours</span>
+              </div>
+            )}
+
+            <div className="left">
+              <div id="ticket-infos">
+                <span>Nom : {tickets.length > 0 ? currentTicket.customer.lastName : ''} </span>
+                <span>Prénom : {tickets.length > 0 ? currentTicket.customer.firstName : ''}</span>
+                <span>Horaire estimé :&nbsp; {tickets.length > 0 ? currentTicket.estimatedEntryTime.substring(
+                  currentTicket.estimatedEntryTime.indexOf('T') + 1,
+                  currentTicket.estimatedEntryTime.indexOf('T') + 6,
+                ) : ''}
+                </span>
+
+                <div id="ticket-ref">
+                  <span>Ref ticket : </span>
+                  <span className="ref"> {tickets.length > 0 ? currentTicket.id : ''}</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="right">
+              <div id="covers-nb">
+                <span className="covers-title">Nombre de couverts&nbsp;:</span>
+                <span className="covers">{tickets.length > 0 ? currentTicket.coversNb : ''}<i class="fa fa-cutlery" aria-hidden="true"></i></span>
+                
+                <div className="add-ticket">
+                  <span id="confirm" onClick={handleConfirm}>Placé</span>
+                  <span id="cancel" onClick={handleCancel}>Absent</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div id="admin-bottom-section" style={mobileClockClickState == 'none' ? {display: 'none'} : {display: 'flex'}}>
+            <div className="first">
+              <h3>Temps d'attente actuel&nbsp;:</h3>
+
+              <div className="estimate-waiting-time">
+                {(status === 1) && <span id="less-time" onClick={handleRemoveClick} />}
+
+                <div id="time-container">
+                  <span id="time">{averageEatingTime}</span>
+                </div>
+
+                {(status === 1) && <span id="more-time" onClick={handleAddClick} />}
+              </div>
+            </div>
+            <div className="second">
+              <h3>Total en attente : </h3>
+                <span id="tickets-count">{tickets.length}{(tickets.length < 2 ? ' ticket' : ' tickets')}</span>
+            </div>
+          </div>
+        </div>
+
+        {/* right section */}
+        <div id="right-section">
+          <div id="admin-side-section">
+            <ul id="ticket-list">
+              {tickets.length === 0 && (
+                <div id="empty-list-add-ticket">
+                  {(status === 0) && <span id="service-off">Pour ajouter un ticket, lancez le service.</span>}
+
+                  {(status === 1) && (
+                    <>
+                      <span>Pour ajouter un ticket, c'est ici</span>
+                      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.proctorgallagherinstitute.com%2Fwp-content%2Fuploads%2F2019%2F03%2Forange-curved-arrow.png&f=1&nofb=1"/>
+                    </>
+                  )}
+                </div>
+              )}
+
+              {tickets.map((ticket) => (
+                <Ticket key={ticket.id} ticket={ticket} />
+              ))}
+            </ul>
+
+            <div className="tickets-count">
+              {(status === 0) && <span id="service-off">Service à l'arrêt</span>}
+              <span id="mobile-time-button" onClick={handleMobileClockButtonClick}><i class="fa fa-clock-o" aria-hidden="true"></i></span>
+              {(status === 1) && <span id="add-ticket" onClick={openModal}>Ajouter un ticket</span>}
+
+              <Modal
+                isOpen={open}
+                onAfterOpen={handleOpen}
+                onRequestClose={handleTicketCancel}
+                onAfterClose={handleOnAfterClose}
+                style={customStyles}
+                shouldFocusAfterRender
+                shouldCloseOnEsc
+                contentLabel="Ticket adding modal" // for screenreaders
+              >
+                <div className="modal-title">
+                  {showModalTicketValidation && <div className="previous-button" onClick={handleShowModalTicketForm} />}
+                  {!showModalTicketValidation && <div className="close-button" onClick={handleTicketCancel}>Fermer {/* &#x274C; */} </div>}
+                </div>
+
+                <div className="ticket-form--container">
+                  {!showModalTicketValidation && <h2 ref={_modalTitle => (modalTitle = _modalTitle)}>Ajout de ticket</h2> }
+
+                  <form className="ticket-form" onSubmit={handleSubmit}>
+                    {!showModalTicketValidation && (
+                      <>
+                        <Field
+                          name="lastName"
+                          placeholder="Nom"
+                          onChange={changeTicketInputValue}
+                          value={lastName}
+                        />
+
+                        <Field
+                          name="firstName"
+                          placeholder="Prénom"
+                          onChange={changeTicketInputValue}
+                          value={firstName}
+                        />
+
+                        <Field
+                          name="email"
+                          type="email"
+                          placeholder="Adresse Email"
+                          onChange={changeTicketInputValue}
+                          value={email}
+                        />
+
+                        <Field
+                          name="phone"
+                          placeholder="Téléphone"
+                          onChange={changeTicketInputValue}
+                          value={phone}
+                        />
+
+                        <Field
+                          name="cutlery"
+                          type="number"
+                          placeholder="Nombre de couverts"
+                          onChange={changeTicketInputValue}
+                          value={cutlery}
+                          step="5"
+                          min="0"
+                        />
+
+                        { modalErrors && (
+                          <div id="errors">
+                            <span> Merci de compléter les champs <strong>Nom</strong>, <strong>Prénom</strong> et <strong>Nombre de couverts</strong>.</span>
+                          </div>
+                        )}
+
+
+                        { emailError && (
+                          <div id="errors">
+                            <span> Merci de vérifier l'adresse mail.</span>
+                          </div>
+                        )}
+
+                        {errors && errors.field=='coversNb' && (
+                          <span className="cover-error">{errors.message}</span>
+                        )}
+                      </>
+                    )}
+
+                    { showModalTicketValidation && (
+                      <>
+                        <h2 ref={_modalTitle => (modalTitle = _modalTitle)}>Récapitulatif</h2>
+                        <h3>Prénom</h3>
+                        <span>{lastName}</span>
+
+                        <h3>Nom</h3>
+                        <span>{firstName}</span>
+ 
+                        {(email.length > 1) && (
+                          <>
+                            <h3>Email</h3>
+                            <span>{email}</span>
+                          </>
+                        )}
+
+                        {(phone.length > 1) && (
+                          <>
+                            <h3>Téléphone</h3>
+                            <span>{phone}</span>
+                          </>
+                        )}
+
+                        <h3>Nombre de couverts</h3>
+                        <span>{cutlery}</span>
+
+                        <h3>Heure estimée</h3>
+                        <span id="estimated-entry">{estimatedEntryTime}</span>
+
+                        <div className="bottom">
+                          <div className="cancel-button" onClick={handleTicketCancel}>Annuler</div>
+                          <button className="ticket-submit button-alt" type="submit">Valider</button>
+                        </div>
+                      </>
+                    )}
+
+                    { !showModalTicketValidation && (
+                      <div className="bottom">
+                        <a className="ticket-next button-alt" onClick={handleTicketValidation}>
+                          Suivant
+                        </a>
+                      </div>
+                    )}
+                  </form>
+                </div>
+              </Modal>
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
     ))
   );
 };
