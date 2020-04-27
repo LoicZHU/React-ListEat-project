38a27b83940ee1025fd9549d6235dc9dc43c9a0c
Liza-T-XLS <shapi94@gmail.com>
2020-04-27 12:33:39 +0200

    checks if logged partner is same as in URL

--- backend/src/Controller/Api/TicketController.php
+++ backend/src/Controller/Api/TicketController.php
@@ -19,6 +19,7 @@ use Symfony\Component\Messenger\MessageBus;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class TicketController extends AbstractController
 {
@@ -228,15 +229,22 @@ class TicketController extends AbstractController
 
     }
 
-     /**
-     * @Route("/api/partner/{id<\d+>}/tickets", name="api_tickets_show", methods={"GET"})
-     */
+    /**
+    * @Route("/api/partner/{id<\d+>}/tickets", name="api_tickets_show", methods={"GET"})
+    * @IsGranted("ROLE_RESTAURATEUR")
+    */
     public function showAll($id, TicketRepository $ticketRepository, RestaurantRepository $restaurantRepository)
     {
         $restaurant = $restaurantRepository->find($id);
 
         if (!$restaurant) {
             return $this->json(['message' => 'Ce restaurant n\'existe pas.'], Response::HTTP_NOT_FOUND);
+        }
+
+        // checks if the connected partner is the same as the owner of the restaurant on which he/she wants to perform an action
+        $user = $this->getUser();
+        if ($user->getRestaurant()->getId() != $id) {
+            return $this->json(['message' => 'Ce n\'est pas votre restaurant.'], Response::HTTP_BAD_REQUEST);
         }
         
         // finds all active tickets related to a single restaurant, ordered by estimatedEntryTime, sorted in ascending order
@@ -247,6 +255,7 @@ class TicketController extends AbstractController
 
     /**
      * @Route("api/partner/{id<\d+>}/tickets/{ticketId<\d+>}", name="api_tickets_partner_edit", methods={"PUT"})
+     * @IsGranted("ROLE_RESTAURATEUR")
      */
     public function partnerEdit($id, $ticketId, Request $request, TicketRepository $ticketRepository, CustomerRepository $customerRepository, RestaurantRepository $restaurantRepository)
     {
@@ -264,7 +273,12 @@ class TicketController extends AbstractController
             return $this->json(['Ce ticket n\'existe pas.'], Response::HTTP_NOT_FOUND);
         }
         $ticket = $ticket[0];
-        
+
+        // checks if the connected partner is the same as the owner of the restaurant on which he/she wants to perform an action
+        $user = $this->getUser();
+        if ($user->getRestaurant()->getId() != $id) {
+            return $this->json(['message' => 'Ce n\'est pas votre restaurant.'], Response::HTTP_BAD_REQUEST);
+        }
         // if the ticket was created by the restaurateur, the below condition can be used to confirm the ticket and add the customer to the waiting list (thus not sending an email to the customer, contrary to the validation made by the client him/herself)
         if ($data->status == "confirmed") {
             $ticket->setStatus(1);
